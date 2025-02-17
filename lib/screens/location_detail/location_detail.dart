b40999d8ee0e2585f4754d029c58d566249c9bba
pivnati <natanmuleta911@gmail.com>
2025-02-17 11:30:07 -0700

    create the app.dart

---
+++ lib/screens/location_detail/location_detail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class LocationDetail extends StatelessWidget {
+  const LocationDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
